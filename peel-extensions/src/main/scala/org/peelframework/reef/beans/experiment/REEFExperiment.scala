65c069c31c95a79c6facbd4433e14bb305e7bac4
Christoph Bruecke <christoph.bruecke@campus.tu-berlin.de>
2016-02-24 12:35:50 +0100

    Implement REEFExperiment

--- peel-extensions/src/main/scala/org/peelframework/reef/beans/experiment/REEFExperiment.scala
+++ peel-extensions/src/main/scala/org/peelframework/reef/beans/experiment/REEFExperiment.scala
@@ -1,19 +1,39 @@
+/**
+ * Copyright (C) 2014 TU Berlin (peel@dima.tu-berlin.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.peelframework.reef.beans.experiment
+
+import java.io.FileWriter
+import java.nio.file.{Paths, Files}
 
 import com.typesafe.config.Config
 import org.peelframework.core.beans.data.{ExperimentOutput, DataSet}
 import org.peelframework.core.beans.experiment.Experiment
 import org.peelframework.core.beans.experiment.Experiment.Run
 import org.peelframework.core.beans.system.System
+import org.peelframework.core.util.shell
+import org.peelframework.hadoop.beans.system.Yarn
 import org.peelframework.reef.beans.experiment.REEFExperiment.SingleJobRun
-import org.peelframework.reef.beans.system.REEF
+import spray.json._
 
 /**
-  * Created by carabolic on 18/02/16.
+  * An `Experiment` implementation that handles the execution of a single REEF job.
   */
 class REEFExperiment(command: String,
                      systems: Set[System],
-                     runner : REEF,
+                     runner : Yarn,
                      runs   : Int,
                      inputs : Set[DataSet],
                      outputs: Set[ExperimentOutput],
@@ -26,38 +46,72 @@ class REEFExperiment(command: String,
     * @param force Force execution of this run
     * @return An run for this experiment identified by the given `id`
     */
-  override def run(id: Int, force: Boolean): Run[REEF] = new SingleJobRun(id, this, force)
+  override def run(id: Int, force: Boolean): Run[Yarn] = new SingleJobRun(id, this, force)
 
   /** Copy the object with updated name and config values.
     *
     * @param name The updated name value.
     * @param config The updated config value.
     */
-  override def copy(name: String, config: Config): Experiment[REEF] =
+  override def copy(name: String, config: Config): Experiment[Yarn] =
     new REEFExperiment(command, systems, runner, runs, inputs, outputs, name, config)
 }
 
 object REEFExperiment {
 
   case class REEFState( runnerID: String,
-                        runTime: Long,
-                        runExitCode: Option[Int],
+                        runnerName: String,
                         runnerVersion: String,
-                        runnerName: String) extends Experiment.RunState
+                        var runTime: Long = 0,
+                        var runExitCode: Option[Int] = None) extends Experiment.RunState
+
+  object REEFStateProtocol extends DefaultJsonProtocol with NullOptions {
+    implicit val reefStateFormat = jsonFormat5(REEFState)
+  }
 
   class SingleJobRun( val id: Int,
                       val exp: REEFExperiment,
-                      val force: Boolean) extends Experiment.SingleJobRun[REEF, REEFState] {
+                      val force: Boolean) extends Experiment.SingleJobRun[Yarn, REEFState] {
 
-    override protected def loadState(): REEFState = ???
+    import REEFStateProtocol._
 
-    override protected def writeState(): Unit = ???
+    override protected def loadState(): REEFState = {
+      if (Files.isRegularFile(Paths.get(s"$home/state.json"))) {
+        try {
+          scala.io.Source.fromFile(s"$home/state.json").mkString.parseJson.convertTo[REEFState]
+        } catch {
+          case e: Throwable => REEFState(exp.runner.beanName, exp.runner.name, exp.runner.version)
+        }
+      } else {
+        REEFState(exp.runner.beanName, exp.runner.name, exp.runner.version)
+      }
+    }
 
-    override protected def runJob(): Unit = ???
+    override protected def writeState(): Unit = {
+      val fw = new FileWriter(s"$home/state.json")
+      fw.write(state.toJson.prettyPrint)
+      fw.close()
+    }
 
-    override protected def cancelJob(): Unit = ???
+    override protected def runJob(): Unit = {
+      val (exitCode, runTime) = Experiment.time(this !(s"${command.trim}", s"$home/run.out", s"$home/run.err"))
+      state.runExitCode = Some(exitCode)
+      state.runTime = runTime
+    }
+
+    override protected def cancelJob(): Unit = {} // NOP - do nothing
 
     /** Check if the execution of this run exited successfully. */
-    override def isSuccessful: Boolean = ???
+    override def isSuccessful: Boolean = state.runExitCode.getOrElse(-1) == 0
+
+    private def !(command: String, outFile: String, errFile: String) = {
+      val yarnHome = exp.config.getString(s"system.${exp.runner.configKey}.path.home")
+      // get the yarn classpath, such that REEF knows about
+      val yarnClasspath = (shell !! s"$yarnHome/bin/yarn classpath").trim
+      // add the $BUNDLE_BIN/apps directory
+      val jarHome = exp.config.getString("app.path.apps")
+      val libDir = s"${exp.config.getString("app.path.home")}/lib"
+      shell ! s"java -cp $yarnClasspath:$libDir/*:$jarHome/* ${command.trim} > $outFile 2> $errFile"
+    }
   }
 }
